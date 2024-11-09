0bd79cc5129a57fef01085e001326c619ef04dae
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-11-10 01:19:07 +0530

    latest commits

--- api/index.js
+++ api/index.js
@@ -25,10 +25,10 @@ app.use("/dashboard", dashboardRoutes);
 app.use("/savings", savingsRoutes);
 app.use("/investments", investmentsRoutes);
 app.use("/transactions", transactionRoutes);
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist' , 'index.html'));
-})
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
