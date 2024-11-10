0e6abfb1606beb8f590d2bb7d4f63773e017ed26
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-11-10 17:00:51 +0530

    updated vercel json file

--- api/index.js
+++ api/index.js
@@ -41,7 +41,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-// Set the port from environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
 
 // Start the server
